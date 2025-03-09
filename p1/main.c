d79008b86f3e7787a058b872ef7d385a7865f27e
GoscZnickiem <maciek.6098@gmail.com>
2025-03-09 16:54:34 +0100

    there is literally no need to optimise this pls stop

--- p1/main.c
+++ p1/main.c
@@ -1,9 +1,28 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#ifndef SPRAWDZACZKA
+#include <time.h>
+#endif
 
 static uint64_t* graph;
 static uint32_t outEdges[10001] = {0};
+
+static char* buffer;
+static size_t pos = 0;
+
+uint64_t readInt() {
+	uint64_t num = 0;
+	while (buffer[pos] < '0' || buffer[pos] > '9') pos++;
+	while (buffer[pos] >= '0' && buffer[pos] <= '9') {
+		num = num * 10 + (buffer[pos] - '0');
+		pos++;
+	}
+	return num;
+}
 
 int DFS(uint32_t vert, uint32_t depth) {
 	if(!vert && depth) {
@@ -25,25 +44,41 @@ int DFS(uint32_t vert, uint32_t depth) {
 }
 
 int main(void) {
-	size_t n = 0;
-	scanf("%zu\n", &n);
+
+#ifndef SPRAWDZACZKA
+	clock_t start, end;
+	double cpu_time_used;
+	start = clock();
+#endif
+
+	struct stat sb;
+	fstat(STDIN_FILENO, &sb);
+	buffer = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, STDIN_FILENO, 0);
+    if (buffer == MAP_FAILED) {
+        return 1;
+    }
+
+	size_t n = readInt();
 
 	uint64_t graphi[n+1];
 	graph = graphi;
 
-	char line[64];
 	for (size_t i = 1; i <= n; i++) {
-		fgets(line, 64, stdin);
-		char* ptr = line;
-		uint64_t l = strtoul(ptr, &ptr, 10);
-		uint64_t m = strtoul(ptr, &ptr, 10);
-		uint64_t r = strtoul(ptr, &ptr, 10);
+		uint64_t l = readInt();
+		uint64_t m = readInt();
+		uint64_t r = readInt();
 		uint64_t next = outEdges[r];
 		graph[i] = l | (m << 16) | (next << 32);
 		outEdges[r] = i;
 	}
 
 	if(!DFS(0, 0)) printf("BRAK\n");
+
+#ifndef SPRAWDZACZKA
+	end = clock();
+	cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+	printf("Time taken: %f seconds\n", cpu_time_used);
+#endif
 
 	return 0;
 }
