8e1d925d9b2202f251492df1bc9a0c5feeeb0258
GoscZnickiem <maciek.6098@gmail.com>
2025-03-09 16:33:46 +0100

    unnecesarry performance increase

--- p1/main.c
+++ p1/main.c
@@ -1,65 +1,49 @@
-#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
-#ifndef SPRAWDZACZKA
-#include <time.h>
-#endif
+#include <stdlib.h>
 
-uint64_t* graph;
-uint32_t dest[10001] = {0};
+static uint64_t* graph;
+static uint32_t outEdges[10001] = {0};
 
-int DST(uint32_t entry, uint32_t depth) {
-	if(!entry && depth) {
+int DFS(uint32_t vert, uint32_t depth) {
+	if(!vert && depth) {
 		printf("%u\n", depth);
 		return 1;
 	}
-	if(!dest[entry]) return 0;
 
-	uint32_t d = dest[entry];
-	dest[entry] = 0;
-	uint32_t i = d;
-	do {
-		if(DST(graph[i] & 0x3FFF, depth + 1)) {
-			printf("%lu %lu %u\n", graph[i] & 0x3FFF, (graph[i] >> 14) & 0x3FFF, entry);
+	uint32_t i = outEdges[vert];
+	outEdges[vert] = 0;
+	while (i != 0) {
+		if(DFS(graph[i] & 0xFFFF, depth + 1)) {
+			printf("%lu %lu %u\n", graph[i] & 0xFFFF, (graph[i] >> 16) & 0xFFFF, vert);
 			return 1;
 		}
-		i = graph[i] >> 46;
-	} while (i != d);
+		i = graph[i] >> 32;
+	}
 
 	return 0;
 }
 
 int main(void) {
-#ifndef SPRAWDZACZKA
-	clock_t start, end;
-	double cpu_time_used;
-	start = clock();
-#endif
-
 	size_t n = 0;
-	scanf("%zu", &n);
+	scanf("%zu\n", &n);
 
 	uint64_t graphi[n+1];
 	graph = graphi;
 
+	char line[64];
 	for (size_t i = 1; i <= n; i++) {
-		uint64_t l, m, r;
-		scanf("%lu %lu %lu", &l, &m, &r);
-		uint64_t prev = dest[r] ? (graph[dest[r]] >> 28) & 0x3FFFF : i;
-		dest[r] = dest[r] ? dest[r] : i;
-		uint64_t next = dest[r];
-		graph[prev] = (graph[prev] & 0x00003FFFFFFFFFFF) | (i << 46);
-		graph[next] = (graph[next] & 0xFFFFC0000FFFFFFF) | (i << 28);
-		graph[i] = l | (m << 14) | (prev << 28) | (next << 46);
+		fgets(line, 64, stdin);
+		char* ptr = line;
+		uint64_t l = strtoul(ptr, &ptr, 10);
+		uint64_t m = strtoul(ptr, &ptr, 10);
+		uint64_t r = strtoul(ptr, &ptr, 10);
+		uint64_t next = outEdges[r];
+		graph[i] = l | (m << 16) | (next << 32);
+		outEdges[r] = i;
 	}
 
-	if(!DST(0, 0)) printf("BRAK\n");
-
-#ifndef SPRAWDZACZKA
-	end = clock();
-	cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-    printf("Time taken: %f seconds\n", cpu_time_used);
-#endif
+	if(!DFS(0, 0)) printf("BRAK\n");
 
 	return 0;
 }
