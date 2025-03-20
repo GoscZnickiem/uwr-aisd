fccbafb41d4adbfe8b98e4537bce09ab11d5419d
GoscZnickiem <maciek.6098@gmail.com>
2025-03-20 17:05:57 +0100

    bruh it done

--- p2/main.c
+++ p2/main.c
@@ -1,61 +1,85 @@
 #include <stdint.h>
 #include <stdio.h>
-
-typedef uint32_t uint;
-
-#define MIN(a, b) (a < b ? a : b)
-#define MAX(a, b) (a > b ? a : b)
+#ifndef SPRAWDZACZKA
+#include <time.h>
+#endif
 
 typedef struct {
-	uint list;
-	uint index;
+	uint32_t list;
+	uint32_t index;
 } Elem;
 
-static uint n;
+static uint32_t M, K;
+static uint32_t n;
 static Elem* heap;
 
 #define VAL(i) ((uint64_t)heap[i].list * (uint64_t)(heap[i].list - heap[i].index))
 #define SWAP(i, j) { Elem tmp = heap[i]; heap[i] = heap[j]; heap[j] = tmp; }
 
-void moveDown(uint i) {
-	uint k = i;
-	uint j;
+void moveDown(uint32_t i) {
+	uint32_t k = i;
+	uint32_t j;
 	do {
 		j = k;
-		if(2 * j <= n && VAL(2*j)   < VAL(k)) k = 2 * j;
-		if(2 * j < n  && VAL(2*j+1) < VAL(k)) k = 2 * j + 1;
+		if(2 * j <= n && VAL(2*j)   > VAL(k)) k = 2 * j;
+		if(2 * j < n  && VAL(2*j+1) > VAL(k)) k = 2 * j + 1;
 		SWAP(j, k);
-	} while(j == k);
+	} while(j != k);
 }
 
-void moveUp(uint i) {
-	uint k = i;
-	uint j;
+void moveUp(uint32_t i) {
+	uint32_t k = i;
+	uint32_t j;
 	do {
 		j = k;
-		if(j > 1 && VAL(j >> 1) > VAL(k)) k = j >> 1;
+		if(j > 1 && VAL(j >> 1) < VAL(k)) k = j >> 1;
 		SWAP(j, k);
-	} while(j == k);
+	} while(j != k);
+}
+
+void push(uint32_t list) {
+	n++;
+	heap[n].list = list;
+	heap[n].index = 0;
+	moveUp(n);
 }
 
 void popAndPushNext() {
-	heap[1].index--;
+	static uint64_t last = 0;
+	uint64_t v = VAL(1);
+	if(v != last) {
+		printf("%zu\n", v);
+		last = v;
+		K--;
+	}
+	uint32_t index = ++heap[1].index;
 	moveDown(1);
-}
-
-void push(uint list) {
-	heap[n].list = list;
-	heap[n].index = 0;
-	n++;
-	moveUp(n - 1);
+	if(index >= n) push(M - n);
 }
 
 int main(void) {
-	uint64_t M, k;
-	scanf("%zu %zu", &M, &k);
+#ifndef SPRAWDZACZKA
+	clock_t start, end;
+	double cpu_time_used;
+	start = clock();
+#endif
+	scanf("%u %u", &M, &K);
 
-	Elem heapi[MIN(M, k) + 1];
+	Elem heapi[(M < K ? M : K) + 1];
 	heap = heapi;
+
+	heap[1].list = M; 
+	heap[1].index = 0; 
+	n = 1;
+
+	setvbuf(stdout, NULL, _IOFBF, 1<<20);
+	while(K > 0) popAndPushNext();
+
+#ifndef SPRAWDZACZKA
+	end = clock();
+	cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+	printf("Time taken: %f seconds\n", cpu_time_used);
+#endif
 
 	return 0;
 }
